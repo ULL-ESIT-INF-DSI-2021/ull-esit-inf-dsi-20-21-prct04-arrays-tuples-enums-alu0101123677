1693bf93d8921db2c36291696aa047e0f25555b9
Bruno Arroyo <alu0101123677@ull.edu.es>
2021-03-11 13:21:50 +0000

    ejercicio-3

---
+++ src/ejercicio-3.ts
@@ -0,0 +1,29 @@
+function meanAndConcatenate(array: string[]): string[] {
+  let str: string = '0123456789'
+  let str2 : string = 'abcdefghijklmnñopqrstuvwxyzABCDEFGHIJKLMNÑOPQRSTUVWXYZ'
+  let number: number = 0;
+  let aux: string = '';
+  let array2 = ['', ''];
+  let count = 0;
+
+  for (let i = 0; i < array.length; i++) {
+    if (str.search(array[i]) != -1) {
+      number = number + +array[i];
+      count++;
+    }  
+    if (str2.search(array[i]) != -1)
+      aux = aux + array[i];
+  }
+  
+  number = number / count;
+  
+  array2[0] = String(number);
+  array2[1] = aux;
+
+  console.log(array2[0]);
+  console.log(array2[1]);
+
+  return array2;
+}
+  
+meanAndConcatenate(['u', '6', 'd', '1', 'i', 'w', '6', 's', 't', '4', 'a', '6', 'g', '1', '2', 'w', '8', 'o', '2', '0']);
