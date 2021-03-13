b543c93609190fea5bc8339a4707d8931ad7b141
Bruno Arroyo <alu0101123677@ull.edu.es>
2021-03-13 15:12:20 +0000

    test unitarios

--- tests/ejercicio-2.spec.ts
+++ tests/ejercicio-2.spec.ts
@@ -3,7 +3,7 @@ import {expect} from 'chai';
 import {meshArray} from '../src/ejercicio-2';
 
 describe('Función que detecta si hay palabras concatenadas', () => {
-  it('("["allow", "lowering", "ringmaster", "terror"]") returns value lowringter', () => {
+  it('(["allow", "lowering", "ringmaster", "terror"]) returns value lowringter', () => {
     expect(meshArray(['allow', 'lowering', 'ringmaster', 'terror'])).to.be.equal('lowringter')
   });
 });
