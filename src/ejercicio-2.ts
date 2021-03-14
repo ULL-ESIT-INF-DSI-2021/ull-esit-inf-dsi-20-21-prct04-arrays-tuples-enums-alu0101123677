540f0a00422bfde5239aa91067f235ca49cf8de8
Bruno Arroyo <alu0101123677@ull.edu.es>
2021-03-14 16:24:30 +0000

    version final

--- src/ejercicio-2.ts
+++ src/ejercicio-2.ts
@@ -1,28 +1,28 @@
 /**
  * Función que detecta si hay palabras concatenadas
  * @param array recibe un array de string con diversas palabras 
- * @returns retorna un string vacio o con las partes encadenadas del array
+ * @returns retorna un string con un mensaje de error o con las partes encadenadas del array
  */
 
 export function meshArray(array: string[]): string {
   let str: string = '';
 
   for(let i = 0; i < array.length - 1; i++) {
-    const char: string = array[i];
-    const next_char: string = array[i + 1];
-    let position: number = char.length -1;
+    const palabra: string = array[i];
+    const siguiente_palabra: string = array[i + 1];
+    let position: number = palabra.length -1;
     let next_position: number = 0;
     
-    while (char[position] != next_char[next_position]) {
+    while (palabra[position] != siguiente_palabra[next_position]) {
       position--;
       if (position < 0) {
         return 'Error al encadenar';
       } 
     }
 
-    for(let j: number = position; j < char.length; j++) {
-      if(char[j] == next_char[next_position]) {
-        str = str + char[j];
+    for(let j: number = position; j < palabra.length; j++) {
+      if(palabra[j] == siguiente_palabra[next_position]) {
+        str = str + palabra[j];
         next_position++;
       }
       else {
