0fdc5563ae993ba774842be24f13417111aa9781
Bruno Lorenzo Arroyo Pedraza <72444930+alu0101123677@users.noreply.github.com>
2021-03-14 16:33:54 +0000

    Delete tests directory

--- tests/ejercicio-2.spec.ts
+++
@@ -1,9 +0,0 @@
-import 'mocha';
-import {expect} from 'chai';
-import {meshArray} from '../src/ejercicio-2';
-
-describe('Función que detecta si hay palabras concatenadas', () => {
-  it('(["allow", "lowering", "ringmaster", "terror"]) returns value lowringter', () => {
-    expect(meshArray(['allow', 'lowering', 'ringmaster', 'terror'])).to.be.eql('lowringter')
-  });
-});
--- tests/ejercicio-8.spec.ts
+++
@@ -1,9 +0,0 @@
-import 'mocha';
-import {expect} from 'chai';
-import {agent} from '../src/ejercicio-8';
-
-describe('Función que calcula los movimientos que se tienen que hacer desde la posición inicial a la final', () => {
-  it('(30, 50, [2, 2], [5, 3]) returns value [ "East", "East", "East", "North"]', () => {
-    expect(agent(30, 50, [2, 2], [5, 3])).to.be.eql(['East', 'East', 'East', 'North'])
-  });
-});
